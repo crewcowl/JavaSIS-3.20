50a3ef24e482a0d1027ec12453ee9310044347a1
Lev <ld.kryukov@gmail.com>
2020-03-22 23:38:32 +0700

    Unit 5.1 solution

--- unit5/unit5-homework/src/main/java/pro/sisit/javacourse/PathFinder.java
+++ unit5/unit5-homework/src/main/java/pro/sisit/javacourse/PathFinder.java
@@ -1,9 +1,14 @@
 package pro.sisit.javacourse;
 
 import pro.sisit.javacourse.optimal.DeliveryTask;
+import pro.sisit.javacourse.optimal.Route;
+import pro.sisit.javacourse.optimal.RouteType;
 import pro.sisit.javacourse.optimal.Transport;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PathFinder {
 
@@ -13,7 +18,17 @@ public class PathFinder {
      * Если список transports равен null, то оптимальеый транспорт тоже равен null.
      */
     public Transport getOptimalTransport(DeliveryTask deliveryTask, List<Transport> transports) {
-        // ToDo: realize me!
-        return null;
+        if(deliveryTask != null & transports != null) {
+            List<Route> enableRoutes = deliveryTask.getRoutes();
+            Map<RouteType, BigDecimal> mapEnableRoutes = enableRoutes.stream()
+                    .collect(Collectors.toMap(Route::getType, Route::getLength));
+
+            return transports.stream()
+                    .filter(transport -> transport.getVolume().compareTo(deliveryTask.getVolume()) >= 0)
+                    .filter(transport -> mapEnableRoutes.containsKey(transport.getType()))
+                    .min((t1, t2) -> t1.getPrice().multiply(mapEnableRoutes.get(t1.getType()))
+                            .compareTo(t2.getPrice().multiply(mapEnableRoutes.get(t2.getType()))))
+                    .get();
+        } else return null;
     }
 }
